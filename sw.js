16bdfad6d8637bceb0bef4ece57b135506d9f485
Ilya <ilya1997marshal@gmail.com>
2026-04-23 17:00:34 +0700

    update

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'ctai-base-v102'; // Обязательно меняйте при каждом деплое
+const CACHE_NAME = 'ctai-base-v100'; // Обязательно меняйте при каждом деплое
 const ASSETS = [
   './',
   './index.html',
